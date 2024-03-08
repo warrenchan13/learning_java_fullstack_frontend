7f5b6ca6311bd1c6a3496ee7fc66c96e61625f2b
Warren Chan <warren.chan0113@gmail.com>
2024-03-08 18:05:49 -0500

    finish edit user page

---
+++ src/users/EditUser.js
@@ -0,0 +1,89 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+
+export default function EditUser() {
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.put(`http://localhost:8080/user/${id}`, {
+      name,
+      username,
+      email,
+    });
+    navigate("/");
+  };
+
+  const loadUser = async () => {
+    const result = await axios.get(`http://localhost:8080/user/${id}`);
+    console.log(result.data);
+    setName(result.data.name);
+    setUsername(result.data.username);
+    setEmail(result.data.email);
+  };
+
+  return (
+    <div className="container">
+      <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+        <h2 className="text-center m-4">Edit User</h2>
+        <form onSubmit={(e) => onSubmit(e)}>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              Name
+            </label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter your name"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="username" className="form-label">
+              Username
+            </label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter your username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter your e-mail address"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="btn btn-outline-primary">
+            Submit
+          </button>
+          <Link className="btn btn-outline-danger mx-2" to="/">
+            Cancel
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+}
